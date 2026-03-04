3877955104c2efbd98528888b8671d982bc4f457
Mono963 <ezeespa78@gmail.com>
2026-03-03 23:59:02 -0300

    Error de redireccion

--- src/config/google-0auth.config.ts
+++ src/config/google-0auth.config.ts
@@ -7,5 +7,5 @@ export default registerAs('GoogleOAuth', () => ({
   clientId: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackUrl: process.env.GOOGLE_CALLBACK_URL,
-  frontendUrl: process.env.FRONTEND_CLOUD_URL,
+  frontendUrl: process.env.FRONTEND_URL,
 }));
